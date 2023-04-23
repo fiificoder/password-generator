dc36803f8d76dd77d75b3f0017e6790fd5de0174
fiificoder <elvisboadi962@gmail.com>
2023-04-23 13:37:25 +0000

    Simple go password generator

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func passwordGenerator(length int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	charset := "dscabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+"
+
+	password := make([]byte, length)
+	for k, _ := range password {
+		password[k] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(password)
+}
+
+func main() {
+	passcode := passwordGenerator(9)
+
+	fmt.Println("Password:", passcode)
+
+}
+
+/* basically I realized another simple sequence of doing it
+
+func main() {
+	password := make([]byte, 10)
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(password)
+
+	fmt.Printf("password: %x", password)
+}
+*/
